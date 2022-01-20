c53b9f8aa7f88ef28c1cc58e596db80f4d13c3f5
Little Polar Bear <34699290+Little-Polar-Bear@users.noreply.github.com>
2022-01-20 18:03:19 +1300

    API upload
    Python written API web app

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import mysql.connector
+
+app = Flask(__name__)
+api = Api(app)
+
+# connect to database
+database = mysql.connector.connect(
+    host="194.195.124.16",
+    user="Access_All", 
+    passwd="F1F@2OlS%",
+    database="MediaContent")
+
+# Add dictionary true to get the table headers.
+# Else it just prints out each row as a list without the mapped (header -> body).
+# eg: WANT {Title: "Harry Potter", Id: 27} NOT [Harry Potter, 27]
+databaseCursor = database.cursor(dictionary=True)   
+
+#TEST SQL QUERY CALL
+class MoviesByCategoryWithLimitMaxOrderedByAudienceScore(Resource):
+    #return all the movie titles from a category in the database
+    def get(self, category, limitMax):
+        query = "SELECT * FROM Movies WHERE Primary_Category = \'" + category  + "\' ORDER BY Audience_Score DESC LIMIT " + str(limitMax)
+        return ExecuteQuery(query)
+
+#PRODUCTION
+class MoviesByCategory(Resource):
+    #return all the movie titles from a category in the database
+    def get(self, category):
+        query = "SELECT * FROM Movies WHERE Primary_Category = \'" + category  + "\' "
+        return ExecuteQuery(query)
+
+#PRODUCTION
+class MovieReviews(Resource):
+    #return all the movie Reviews for a titles id
+    def get(self, movieId):
+        query = "SELECT * FROM MovieReviews WHERE Movie_Id = " + str(movieId)
+        return ExecuteQuery(query)
+
+
+# Execute query and return the result with a 200 success code.
+def ExecuteQuery(query):
+    databaseCursor.execute(query)
+    result = databaseCursor.fetchall()
+
+    return result, 200  # return data and 200 OK
+
+
+
+#End Points
+api.add_resource(MovieReviews, '/<int:movieId>/moviereviews')  # this is our url entry point
+api.add_resource(MoviesByCategory, '/<string:category>/movies')  # this is our url entry point
+
+if __name__ == '__main__':
+    app.run()  # run our Flask app
